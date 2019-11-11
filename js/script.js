90c1ba9fbf4839b53a35837b774b543631b8b1f2
murrengan <180107250@stu.sdu.edu.kz>
2019-11-12 00:30:25 +0600

    I fixed the site. At first, the site was written in Russian and corrected in English. Also added courses. made them and also the home page adaptive

--- js/script.js
+++ js/script.js
@@ -15,3 +15,7 @@ scroll(6500, 'shethik-cifra1', 20, 30);
 scroll(12, 'shethik-cifra2', 300, 1);
 scroll(21, 'shethik-cifra3', 250, 1);
 scroll(12, 'shethik-cifra4', 300, 1);
+scroll(110, 'shethik-cifra5', 300, 1);
+scroll(15, 'shethik-cifra6', 300, 1);
+scroll(3, 'shethik-cifra7', 300, 1);
+scroll(12, 'shethik-cifra8', 300, 1);
