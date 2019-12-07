303c79b6a26adf531b67b99a66cf96f87397feb4
ISLAMBEKTOG <49253070+ISLAMBEKTOG@users.noreply.github.com>
2019-12-07 19:36:57 +0600

    New functionals for price block

---
+++ js/priceup.js
@@ -0,0 +1,16 @@
+document.getElementById("ex2").addEventListener("click",openP);
+document.getElementById("close").addEventListener("click",closeP);
+function openP(){
+    var popup = document.getElementById("popup1");
+    popup.style.display = "block";
+}
+function closeP(){
+    var popup = document.getElementById("popup1");
+    popup.style.display = "none";
+}
+document.getElementById("ex3").addEventListener("click",openP);
+document.getElementById("ex4").addEventListener("click",openP);
+document.getElementById("ex5").addEventListener("click",openP);
+document.getElementById("ex6").addEventListener("click",openP);
+document.getElementById("ex7").addEventListener("click",openP);
+
