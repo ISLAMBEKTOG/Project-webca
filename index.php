e4eae416812d0e568dd0cc7b3079a2ffd4fbfe7e
ISLAMBEKTOG <49253070+ISLAMBEKTOG@users.noreply.github.com>
2019-12-07 19:01:45 +0600

    i add new css file

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
     <!-- Animate .css -->
     <link rel="stylesheet" href="css/animate.css">
     <!-- Forma .css -->
-    <link rel="stylesheet" href="css/forma.css">
+    <link rel="stylesheet" href="css/formaa.css">
     <!-- Fontawesome .css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
